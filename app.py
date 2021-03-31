f66c5a207ff19d71805e2d48ce8b7b524eb86d04
Todd Hoy <64391500+Toodoi@users.noreply.github.com>
2021-03-31 12:52:53 +1000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import category_encoders as ce
 from sklearn.preprocessing import MinMaxScaler
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.figure_factory as ff
@@ -94,7 +94,7 @@ def get_model_data(df):
 
     return X_train, X_test, y_train, y_test
 
-# df, scaled_features = get_data()
+df, scaled_features = get_data()
 
 option = st.sidebar.selectbox("Page navigation", ('Home Page', 'Data set', 'Model'))
 
@@ -111,153 +111,153 @@ if option == 'Home Page':
                 'powerful tool for converting Data Science and Machine Learning projects into webapps quickly. You can '
                 'visit my [Github page](https://github.com/Toodoi) for the Python sourcecode.')
 
-# if option == 'Model':
-#     st.header('Random Forest Model')
-#     st.text('I found this dataset on Kaggle.com')
-#
-#
-#     st.header('Time to train the model')
-#     st.text('Here you get to choose the hyperparameters of the model and see how performance changes')
-#     st.text('Adding more trees and allowing them to use more data will increase the time it takes to generate.')
-#     st.text('It may take some time to receive a response when you max these settings.')
-#
-#     sel_col, disp_col = st.beta_columns(2)
-#
-#     max_samples = sel_col.slider('Adjust the maximum data points allowed per tree', min_value=5000, max_value=30000, value=5000, step=5000)
-#
-#     n_estimators = sel_col.slider('Adjust the number of trees in the Random Forest', min_value=1, max_value=20, value=1, step=1)
-#
-#     X_train, X_test, y_train, y_test = get_model_data(df)
-#     rfm = RandomForestRegressor(max_samples=max_samples, n_estimators=n_estimators, max_features=0.5, min_samples_leaf=7)
-#
-#     rfm.fit(X_train, y_train)
-#     preds = rfm.predict(X_test)
-#     rf_preds = np.where(preds > 0.5, 1, 0)
-#
-#     disp_col.subheader('Accuracy of a naive model predicting rain everyday is: 77.1%')
-#     score = round(accuracy_score(y_test, rf_preds), 3)*100
-#     disp_col.subheader(f'Accuracy of the Random Forest model is: {score}%')
-#     disp_col.write()
-#
-#     graph_preds = np.stack([tree.predict(X_test) for tree in rfm.estimators_])
-#
-#     fig, ax = plt.subplots()
-#     gy = [accuracy_score(y_test, np.where(graph_preds[:i + 1].mean(0) > 0.5, 1, 0)) for i in range(n_estimators)]
-#     gx = range(1, n_estimators+1)
-#     ax.plot(gx, gy, marker='s')
-#     ax.set_xticks(gx)
-#     ax.set_xticklabels(gx)
-#     plt.xlabel('Number of Trees')
-#     plt.ylabel('Model Accuracy (%)')
-#     plt.title('Model Accuracy vs Number of Trees in Forest')
-#
-#     if n_estimators > 1:
-#         st.pyplot(fig=fig)
-#
-#     def rf_feat_importance(m, df):
-#         return pd.DataFrame({'Feature': list(df.columns), 'Importance (%)': np.round(m.feature_importances_*100, 1)}
-#                             ).sort_values('Importance (%)', ascending=False)
-#     fi = rf_feat_importance(rfm, X_test)
-#     fi = fi.head(8)
-#     # fig = go.Figure(data=go.Table(columnwidth=[300,100], header=dict(values=list(fi.columns),
-#     #                 fill_color='#FD8E72',
-#     #                 align='center'),
-#     #                 cells=dict(values=[fi['Feature'], fi['Importance (%)']], fill_color='#E5ECF6', align='center')))
-#     # fig.update_layout(margin=dict(l=5,r=5,b=10,t=10), paper_bgcolor='#F5F5F5')
-#     # st.plotly_chart(fig)
-#     st.subheader("Feature importance scores tell us which features in the data set are most important in predicting "
-#                  "the target feature. In this data set Humidity3pm consistently scores very high.")
-#     x = [feature[0] for feature in fi['Feature']]
-#     y = list(fi['Importance (%)'])
-#     fig = go.Figure(data=[go.Bar(x=x, y=y)])
-#     fig.update_layout(title='Feature Importance', yaxis_title_text='Importance (%)', margin=dict(l=5, r=5, b=10, t=30),
-#                       paper_bgcolor='#F5F5F5')
-#     st.plotly_chart(fig)
-#
-#
-# if option == 'Data set':
-#
-#     st.subheader('This data set was produced by the Australian Bureau of Meteorology and contains 10 years of daily '
-#                  'weather observations from many locations across Australia. The data set has 142,193 records and 24 '
-#                  'features excluding the target feature.')
-#
-#     rand_rows = df.iloc[np.random.permutation(len(df))].head(10)
-#     st.subheader('Below are 10 randomly generated rows from the data set. Don\'t forget to scroll across to see more features.'
-#                  ' You can also press the button to generate a new set.')
-#     if st.button('Generate New Rows'):
-#         rand_rows = df.iloc[np.random.permutation(len(df))].head(10)
-#     st.write(rand_rows)
-#
-#     st.subheader("The target feature in the data set is 'RainTomorrow' which has binary yes/no values. We are using the "
-#                  "other 24 features which refer to daily numerical and categorical measurements to attempt to predict whether it "
-#                  "will rain the next day. The chart below shows the target variable is somewhat class imbalanced with 77.1% "
-#                  "of 'no' values and 22.1% 'yes'.")
-#
-#     # RainTomorrow class distribution bar graph
-#     x = list(df.RainTomorrow.value_counts().index)
-#     y = df.RainTomorrow.value_counts()
-#     fig = go.Figure(data=[go.Bar(x=x, y=y, hovertext=[f'{round(y[0]/(y[0]+y[1])*100),1}%', f'{round(y[1]/(y[0]+y[1])*100),1}%'],
-#                                  marker=dict(color=['#e33030', '#7fbf67']))])
-#     fig.update_layout(margin=dict(l=5, r=5, b=10, t=10), paper_bgcolor='#F5F5F5')
-#     st.plotly_chart(fig)
-#
-#     st.subheader('The boxplots below show the distribution for some of the numerical features. I have standardised these features'
-#                  ' with mean 0 and standard deviation of 1 to check if there are any outliers. You can see that Rainfall '
-#                  'and Evaporation are strongly positively skewed with many outliers.')
-#
-#     st.image('Boxplots.png') ## The code created this png
-#     # # Box plots for the scaled features
-#     # N = len(scaled_features.columns)
-#     # c = ['hsl(' + str(h) + ',50%' + ',50%)' for h in np.linspace(0, 360, N)]
-#     # fig = go.Figure()
-#     # for i, feature in enumerate(scaled_features):
-#     #     fig.add_trace(go.Box(x=scaled_features[feature], name=feature, boxpoints='suspectedoutliers', marker_color=c[i],
-#     #                          jitter=0.3, marker_size=1.5))
-#     # fig.update_layout(title='Boxplots of Scaled Numerical Features', margin=dict(l=5, r=5, b=10, t=35),
-#     #     paper_bgcolor='#F5F5F5', showlegend=False
-#     # )
-#     # st.plotly_chart(fig)
-#
-#     st.subheader('For this exercise I am using a Random Forest model for predictions. The Random Forest is an ensemble of decision trees'
-#                  ' which are robust to outliers. If I was using a regression-based model it would be important to try to transform '
-#                  'the distribution of these features to be more normal. I decided to transform the features anyway however '
-#                  'it did not have a great impact on the model\'s accuracy.')
-#
-#     st.subheader('Taking a closer look at the Evaporation feature using a histogram, we can see again that it is highly '
-#                  'skewed to the right. Segmenting by the target variable RainTomorrow, it looks like the lower the '
-#                  'evaporation is for a given day, the less likely it is to rain on the next day.')
-#
-#     st.image('EvapHist.png') ## The code created this png
-#     # # Evaporation histogram
-#     # df3 = df[df.Evaporation < 100]
-#     # fig = px.histogram(df3, x="Evaporation", color="RainTomorrow",
-#     #                    marginal="box",  # or violin, rug
-#     #                    hover_data=df.columns)
-#     # fig.update_layout(xaxis_title_text='Daily Evaporation (mm)', yaxis_title_text='Count',
-#     #                   margin=dict(l=5, r=25, b=5, t=10), paper_bgcolor='#F5F5F5')
-#     # st.plotly_chart(fig)
-#
-#     st.subheader('The last chart we will look at is a histogram of Humidity levels recorded at 3pm. I chose this data '
-#                  'in retrospect because as you will see, it ends up being the most important feature to the Random '
-#                  'Forest for predicting the target feature. As we can see, the histograms segmented by the target '
-#                  'feature below have fairly different distributions which may be why the feature has so much predictive power.')
-#
-#     st.image('HumidHist.png') ## The code created this png
-#     # # Humidity3pm Histogram
-#     # H3pm1 = df[df.RainTomorrow == 'Yes'].Humidity3pm
-#     # H3pm2 = df[df.RainTomorrow == 'No'].Humidity3pm
-#     # fig = go.Figure()
-#     # fig.add_trace(go.Histogram(x=H3pm1, name='Rain Tomorrow'))
-#     # fig.add_trace(go.Histogram(x=H3pm2, name='No Rain Tomorrow'))
-#     #  # Overlay both histograms
-#     # fig.update_layout(barmode='overlay', paper_bgcolor='#F5F5F5', margin=dict(l=5, r=5, b=10, t=35),
-#     #                   xaxis_title_text='Humidity at 3pm', yaxis_title_text='Count')
-#     # # Reduce opacity to see both histograms
-#     # fig.update_traces(opacity=0.75)
-#     # st.plotly_chart(fig)
-#
-#     st.subheader("Now feel free to visit the 'Model' page to see how accurately a Random Forest can predict "
-#                  "next day rain.")
+if option == 'Model':
+    st.header('Random Forest Model')
+    st.text('I found this dataset on Kaggle.com')
+
+
+    st.header('Time to train the model')
+    st.text('Here you get to choose the hyperparameters of the model and see how performance changes')
+    st.text('Adding more trees and allowing them to use more data will increase the time it takes to generate.')
+    st.text('It may take some time to receive a response when you max these settings.')
+
+    sel_col, disp_col = st.beta_columns(2)
+
+    max_samples = sel_col.slider('Adjust the maximum data points allowed per tree', min_value=5000, max_value=30000, value=5000, step=5000)
+
+    n_estimators = sel_col.slider('Adjust the number of trees in the Random Forest', min_value=1, max_value=20, value=1, step=1)
+
+    X_train, X_test, y_train, y_test = get_model_data(df)
+    rfm = RandomForestRegressor(max_samples=max_samples, n_estimators=n_estimators, max_features=0.5, min_samples_leaf=7)
+
+    rfm.fit(X_train, y_train)
+    preds = rfm.predict(X_test)
+    rf_preds = np.where(preds > 0.5, 1, 0)
+
+    disp_col.subheader('Accuracy of a naive model predicting rain everyday is: 77.1%')
+    score = round(accuracy_score(y_test, rf_preds), 3)*100
+    disp_col.subheader(f'Accuracy of the Random Forest model is: {score}%')
+    disp_col.write()
+
+    graph_preds = np.stack([tree.predict(X_test) for tree in rfm.estimators_])
+
+    fig, ax = plt.subplots()
+    gy = [accuracy_score(y_test, np.where(graph_preds[:i + 1].mean(0) > 0.5, 1, 0)) for i in range(n_estimators)]
+    gx = range(1, n_estimators+1)
+    ax.plot(gx, gy, marker='s')
+    ax.set_xticks(gx)
+    ax.set_xticklabels(gx)
+    plt.xlabel('Number of Trees')
+    plt.ylabel('Model Accuracy (%)')
+    plt.title('Model Accuracy vs Number of Trees in Forest')
+
+    if n_estimators > 1:
+        st.pyplot(fig=fig)
+
+    def rf_feat_importance(m, df):
+        return pd.DataFrame({'Feature': list(df.columns), 'Importance (%)': np.round(m.feature_importances_*100, 1)}
+                            ).sort_values('Importance (%)', ascending=False)
+    fi = rf_feat_importance(rfm, X_test)
+    fi = fi.head(8)
+    # fig = go.Figure(data=go.Table(columnwidth=[300,100], header=dict(values=list(fi.columns),
+    #                 fill_color='#FD8E72',
+    #                 align='center'),
+    #                 cells=dict(values=[fi['Feature'], fi['Importance (%)']], fill_color='#E5ECF6', align='center')))
+    # fig.update_layout(margin=dict(l=5,r=5,b=10,t=10), paper_bgcolor='#F5F5F5')
+    # st.plotly_chart(fig)
+    st.subheader("Feature importance scores tell us which features in the data set are most important in predicting "
+                 "the target feature. In this data set Humidity3pm consistently scores very high.")
+    x = [feature[0] for feature in fi['Feature']]
+    y = list(fi['Importance (%)'])
+    fig = go.Figure(data=[go.Bar(x=x, y=y)])
+    fig.update_layout(title='Feature Importance', yaxis_title_text='Importance (%)', margin=dict(l=5, r=5, b=10, t=30),
+                      paper_bgcolor='#F5F5F5')
+    st.plotly_chart(fig)
+
+
+if option == 'Data set':
+
+    st.subheader('This data set was produced by the Australian Bureau of Meteorology and contains 10 years of daily '
+                 'weather observations from many locations across Australia. The data set has 142,193 records and 24 '
+                 'features excluding the target feature.')
+
+    rand_rows = df.iloc[np.random.permutation(len(df))].head(10)
+    st.subheader('Below are 10 randomly generated rows from the data set. Don\'t forget to scroll across to see more features.'
+                 ' You can also press the button to generate a new set.')
+    if st.button('Generate New Rows'):
+        rand_rows = df.iloc[np.random.permutation(len(df))].head(10)
+    st.write(rand_rows)
+
+    st.subheader("The target feature in the data set is 'RainTomorrow' which has binary yes/no values. We are using the "
+                 "other 24 features which refer to daily numerical and categorical measurements to attempt to predict whether it "
+                 "will rain the next day. The chart below shows the target variable is somewhat class imbalanced with 77.1% "
+                 "of 'no' values and 22.1% 'yes'.")
+
+    # RainTomorrow class distribution bar graph
+    x = list(df.RainTomorrow.value_counts().index)
+    y = df.RainTomorrow.value_counts()
+    fig = go.Figure(data=[go.Bar(x=x, y=y, hovertext=[f'{round(y[0]/(y[0]+y[1])*100),1}%', f'{round(y[1]/(y[0]+y[1])*100),1}%'],
+                                 marker=dict(color=['#e33030', '#7fbf67']))])
+    fig.update_layout(margin=dict(l=5, r=5, b=10, t=10), paper_bgcolor='#F5F5F5')
+    st.plotly_chart(fig)
+
+    st.subheader('The boxplots below show the distribution for some of the numerical features. I have standardised these features'
+                 ' with mean 0 and standard deviation of 1 to check if there are any outliers. You can see that Rainfall '
+                 'and Evaporation are strongly positively skewed with many outliers.')
+
+    st.image('Boxplots.png') ## The code created this png
+    # # Box plots for the scaled features
+    # N = len(scaled_features.columns)
+    # c = ['hsl(' + str(h) + ',50%' + ',50%)' for h in np.linspace(0, 360, N)]
+    # fig = go.Figure()
+    # for i, feature in enumerate(scaled_features):
+    #     fig.add_trace(go.Box(x=scaled_features[feature], name=feature, boxpoints='suspectedoutliers', marker_color=c[i],
+    #                          jitter=0.3, marker_size=1.5))
+    # fig.update_layout(title='Boxplots of Scaled Numerical Features', margin=dict(l=5, r=5, b=10, t=35),
+    #     paper_bgcolor='#F5F5F5', showlegend=False
+    # )
+    # st.plotly_chart(fig)
+
+    st.subheader('For this exercise I am using a Random Forest model for predictions. The Random Forest is an ensemble of decision trees'
+                 ' which are robust to outliers. If I was using a regression-based model it would be important to try to transform '
+                 'the distribution of these features to be more normal. I decided to transform the features anyway however '
+                 'it did not have a great impact on the model\'s accuracy.')
+
+    st.subheader('Taking a closer look at the Evaporation feature using a histogram, we can see again that it is highly '
+                 'skewed to the right. Segmenting by the target variable RainTomorrow, it looks like the lower the '
+                 'evaporation is for a given day, the less likely it is to rain on the next day.')
+
+    st.image('EvapHist.png') ## The code created this png
+    # # Evaporation histogram
+    # df3 = df[df.Evaporation < 100]
+    # fig = px.histogram(df3, x="Evaporation", color="RainTomorrow",
+    #                    marginal="box",  # or violin, rug
+    #                    hover_data=df.columns)
+    # fig.update_layout(xaxis_title_text='Daily Evaporation (mm)', yaxis_title_text='Count',
+    #                   margin=dict(l=5, r=25, b=5, t=10), paper_bgcolor='#F5F5F5')
+    # st.plotly_chart(fig)
+
+    st.subheader('The last chart we will look at is a histogram of Humidity levels recorded at 3pm. I chose this data '
+                 'in retrospect because as you will see, it ends up being the most important feature to the Random '
+                 'Forest for predicting the target feature. As we can see, the histograms segmented by the target '
+                 'feature below have fairly different distributions which may be why the feature has so much predictive power.')
+
+    st.image('HumidHist.png') ## The code created this png
+    # # Humidity3pm Histogram
+    # H3pm1 = df[df.RainTomorrow == 'Yes'].Humidity3pm
+    # H3pm2 = df[df.RainTomorrow == 'No'].Humidity3pm
+    # fig = go.Figure()
+    # fig.add_trace(go.Histogram(x=H3pm1, name='Rain Tomorrow'))
+    # fig.add_trace(go.Histogram(x=H3pm2, name='No Rain Tomorrow'))
+    #  # Overlay both histograms
+    # fig.update_layout(barmode='overlay', paper_bgcolor='#F5F5F5', margin=dict(l=5, r=5, b=10, t=35),
+    #                   xaxis_title_text='Humidity at 3pm', yaxis_title_text='Count')
+    # # Reduce opacity to see both histograms
+    # fig.update_traces(opacity=0.75)
+    # st.plotly_chart(fig)
+
+    st.subheader("Now feel free to visit the 'Model' page to see how accurately a Random Forest can predict "
+                 "next day rain.")
 
 
 
